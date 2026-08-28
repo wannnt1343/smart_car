3eabccf05a252cfa968589ffd36a02dc6a02a1b4
wannnet <eyulu1298@163.com>
2026-08-28 14:03:01 +0800

    第四天作业

--- Hi3861/2026.8.28/5.0_Uart_BLE/Uart.c
+++ Hi3861/2026.8.28/5.0_Uart_BLE/Uart.c
@@ -1,56 +1,163 @@
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 #include "ohos_init.h"
 #include "cmsis_os2.h"
+#include "wifiiot_errno.h"
+#include "wifiiot_gpio.h"
+#include "wifiiot_gpio_ex.h"
+#include "wifiiot_adc.h"
+#include "wifiiot_uart.h"
 
-static void thread1(void);
-static void thread2(void);
-/*****任务创建*****/
-static void Hello_World(void)
+//定义枚举类型并定义名字为} MSGQUEUE_OBJ_t
+typedef struct
 {
-    osThreadAttr_t attr;
-    attr.attr_bits = 0U;//设置osThraedJoin是否可以使用
-    attr.cb_mem = NULL;//控制块指针设置
-    attr.cb_size = 0U;//控制块指针大小
-    attr.stack_mem = NULL;//任务栈设置
-    attr.stack_size = 1024 * 4;//任务栈大小
-    //创建任务1
-    attr.name = "thread1";//创建任务名称
-    attr.priority = 25;//任务优先级
-    if (osThreadNew((osThreadFunc_t)thread1, NULL, &attr) == NULL)
+    char *Buf;//对象数据类型
+    uint8_t Idx;
+} MSGQUEUE_OBJ_t;
+MSGQUEUE_OBJ_t  msg;//结构体别名命名为msg
+
+//定义枚举类型并定义名字为} MSGQUEUE_OBJ_t_rx
+//枚举数据：上报数据或者处理命令
+typedef struct
+{
+    char *Buf;//对象数据类型
+    uint8_t Idx;
+} MSGQUEUE_OBJ_t_rx;
+MSGQUEUE_OBJ_t_rx msg_rx;//结构体别名命名为msg_rx
+
+osMessageQueueId_t mid_MsgQueue;   //消息队列id
+osStatus_t status;//创建返回参数
+#define MSGQUEUE_OBJECTS 16//消息队列对象的数量
+#define UART_TASK_STACK_SIZE 1024 * 16//任务堆栈大小
+#define UART_TASK_PRIO 25//任务优先级
+#define UART_BUFF_SIZE 1000//串口发送数据大小
+static const char *data = "Hello, QST!\r\n"; //发送字符串
+
+
+// ==========================================
+// 【新增】在此处提前声明这三个函数，告诉编译器它们存在
+static void UART_Task(void);
+void thread2(void);
+void thread3(void);
+// ==========================================
+
+
+/***创建任务****/
+static void UART_ExampleEntry(void)
+{
+    //创建消息队列
+    mid_MsgQueue = osMessageQueueNew(MSGQUEUE_OBJECTS, 100, NULL);
+    if (mid_MsgQueue == NULL)
     {
-        printf("Falied to create thread1!\n");
+        printf("Falied to create Message Queue!\n");
     }
-    //创建任务2
-    attr.name = "thread2";//创建任务名称
-    attr.priority = 25;//任务优先级
+
+    osThreadAttr_t attr;
+    attr.name = "UART_Task";
+    attr.attr_bits = 0U;
+    attr.cb_mem = NULL;
+    attr.cb_size = 0U;
+    attr.stack_mem = NULL;
+    attr.stack_size = UART_TASK_STACK_SIZE;
+    attr.priority = UART_TASK_PRIO;
+    if (osThreadNew((osThreadFunc_t)UART_Task, NULL, &attr) == NULL)
+    {
+        printf(" Falied to create UART_Task!\n");
+    }
+    
+    attr.name = "thread2";
+    attr.stack_size = UART_TASK_STACK_SIZE;
+    attr.priority = 25;
     if (osThreadNew((osThreadFunc_t)thread2, NULL, &attr) == NULL)
     {
         printf("Falied to create thread2!\n");
     }
-}
-
-/*****任务一*****/
-static void thread1(void)
-{
-while (1)
-{
-printf("任务1正在运行!\n");
-printf("Hello World!\r\n");
-usleep(1000000); //延时1s
-}
-}
-
-/*****任务二*****/
-static void thread2(void)
-{
-    sleep(1);//休眠1秒
-    while (1)
+    
+    attr.name = "thread3";
+    attr.stack_size = 1024;
+    attr.priority = 25;
+    if (osThreadNew((osThreadFunc_t)thread3, NULL, &attr) == NULL)
     {
-        printf("任务2正在运行!\n");
-        printf("Hello QST!\r\n");
-        usleep(3000000);//延时1s
+        printf("Falied to create thread3!\n");
     }
 }
 
-APP_FEATURE_INIT(Hello_World);//启动任务
+
+static void UART_Task(void)
+{
+    uint32_t ret;
+    //GPIO功能初始化
+    GpioInit();
+    //GPIO_00复用为UART1_TXD
+    IoSetFunc(WIFI_IOT_IO_NAME_GPIO_0, WIFI_IOT_IO_FUNC_GPIO_0_UART1_TXD);
+    //GPIO_01复用为UART1_RXD
+    IoSetFunc(WIFI_IOT_IO_NAME_GPIO_1, WIFI_IOT_IO_FUNC_GPIO_1_UART1_RXD);
+    
+    //串口硬件初始化
+    WifiIotUartAttribute uart_attr = {
+        //波特率: 115200
+        .baudRate = 9600,
+        //数据位: 8bits
+        .dataBits = 8,
+        .stopBits = 1,
+        .parity = 0,
+    };
+    
+    //串口功能初始化
+    ret = UartInit(WIFI_IOT_UART_IDX_1, &uart_attr, NULL);
+    if (ret != WIFI_IOT_SUCCESS)
+    {
+        printf("Failed to init uart! Err code = %d\n", ret);
+        return;
+    }
+    
+    printf("UART Test Start\n");
+    while (1)
+    {
+        printf("**************UART_example**************\r\n");
+        //通过串口1发送数据
+        UartWrite(WIFI_IOT_UART_IDX_1, (unsigned char *)data, strlen(data));
+
+        //消息队列接收
+        status = osMessageQueueGet(mid_MsgQueue, &msg_rx, NULL, osWaitForever);
+        if (status == osOK)   printf("Message Queue id:%d, Get msg_rx:%s\n", msg_rx.Idx,msg_rx.Buf);
+        //sleep(1);
+    }
+}
+
+
+void thread2(void)
+{
+    uint8_t  rt;
+    uint8_t  uart_buff[UART_BUFF_SIZE] = {0};
+    uint8_t  *uart_buff_ptr = uart_buff;
+    sleep(1);
+    msg.Idx = 12U;
+    while (1)
+    {   
+        printf("任务2正在运行!\n");
+        //通过串口1接收数据
+        rt=UartRead(WIFI_IOT_UART_IDX_1, uart_buff_ptr, UART_BUFF_SIZE);
+        printf("Uart1 read data:%s\n", uart_buff_ptr);
+        uart_buff_ptr[rt]='\0';      //消息队列中是按字符串进行传输，在我们串口接收数据加个字符串结束符
+        msg.Buf=(char*)uart_buff_ptr ;     //将串口接收到的数据发送到消息队列
+        rt=osMessageQueuePut(mid_MsgQueue, &msg, 0U, 0U);//消息队列发送
+        if(rt==0)
+            printf("Message Queue Send msg:%s\n", msg.Buf);    //发送成功
+        else
+            printf("Message Queue Send msg failed");     //发送失败
+        sleep(1);//延时1秒
+    }
+}
+
+void thread3(void)
+{
+    while (1)
+    {
+        printf("任务3正在运行!\n");
+        sleep(3);//延时3秒
+    }
+}
+
+APP_FEATURE_INIT(UART_ExampleEntry);
